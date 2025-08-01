64715e786a3b9148cc81831c5cee5fb83a79f6ea
Rahul Sharma <rahul@rstforum.net>
2025-08-01 20:49:59 +0530

    Step 13 completed:     - Added MONGO_URI in the .env (MONGO_URI=mongodb://localhost:27017/rststore)     - Installed mongoose     - Added db.config.js config file and used it in the main.js file     - Tested the connection by running the server

--- server/main.js
+++ server/main.js
@@ -1,29 +1,34 @@
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from 'dotenv';
+import express from 'express';
 
-import products from "./data/products.data.js";
+import connectDB from './config/db.config.js';
+import products from './data/products.data.js';
 
 dotenv.config();
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "API is running...",
-  });
+app.get('/', (req, res) => {
+	res.json({
+		message: 'API is running...',
+	});
 });
 
-app.get("/api/v1/products", (req, res) => {
-  res.json(products);
+app.get('/api/v1/products', (req, res) => {
+	res.json(products);
 });
 
-app.get("/api/v1/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+app.get('/api/v1/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
 });
 
 app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}.`);
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${port}.`
+	);
 });
