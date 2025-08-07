5791b440dcf16f0da8e533d491e6c433ca1c868a
Rahul Sharma <rahul@rstforum.net>
2025-08-07 19:46:50 +0530

    Step 24 completed:     - Added products api redux slice     - Modified the home screen to use the productApiSlice and also take in the Loading and Error state

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,30 +1,25 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
 import ProductCard from '@components/ProductCard';
+import { useGetProductsQuery } from '@slices/productApiSlice';
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/v1/products');
-			setProducts(data);
-		};
-
-		fetchProducts();
-	}, []);
+	const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
 	return (
 		<section className='bg-white'>
 			<div className='mx-auto max-w-7xl px-3 py-10 sm:px-6 lg:px-8'>
 				<h1 className='text-2xl font-bold text-slate-900'>Latest Products</h1>
 
-				<div className='grid mt-6 grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-					{products.map((prod) => (
-						<ProductCard key={prod._id} product={prod} />
-					))}
-				</div>
+				{isLoading ? (
+					<p>Loading...</p>
+				) : isError ? (
+					<p>{error?.data?.message || error?.error}</p>
+				) : (
+					<div className='grid mt-6 grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+						{products.map((prod) => (
+							<ProductCard key={prod._id} product={prod} />
+						))}
+					</div>
+				)}
 			</div>
 		</section>
 	);
