00fd0dbd05c3d178a5c7bb94302f02789e059398
Rahul Sharma <rahul@rstforum.net>
2025-08-11 20:12:22 +0530

    Step 32 completed:     - Added remove from cart functionality and hooked it up to the quantity selector dropdown     - Created checkout handler function and attached it to the checkout button

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -24,9 +24,16 @@ const cartSlice = createSlice({
 
 			return updateCart(state);
 		},
+		removeFromCart: (state, action) => {
+			state.cartItems = state.cartItems.filter(
+				(cartItem) => cartItem._id !== action.payload
+			);
+
+			return updateCart(state);
+		},
 	},
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
