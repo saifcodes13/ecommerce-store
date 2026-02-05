7ad74e8eceb9caabf759da463166e725d7a3ff97
Saif Codes <saifcodes13@gmail.com>
2026-02-05 21:24:09 +0530

    small changes

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 		<footer className='w-full'>
 			<div className='mx-auto max-w-7xl px-3 py-6 sm:px-6 lg:px-8'>
 				<p className='text-center text-sm text-slate-700 sm:text-left'>
-					Copyright {new Date().getFullYear()} RST Store. All Rights Reserved.
+					Copyright {new Date().getFullYear()} SaifCart . All Rights Reserved.
 				</p>
 			</div>
 		</footer>
